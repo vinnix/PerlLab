111ed7eda25fc86779ad24790e8198e98bc99962
Vinnix Schmidt <vinicius.schmidt@dextra-sw.com>
2014-09-18 09:01:35 -0300

    Persistência

---
+++ Aula03/sdbm.pl
@@ -0,0 +1,27 @@
+#!/bin/env perl
+#
+#
+use Fcntl; # para certas constantes
+use SDBM_File;
+
+my %dbm;
+my $arqdbm = "demo.dbm";
+
+tie %dbm, 'SDBM_File', $arqdbm, O_CREAT|O_RDWR, 0644;
+
+$dbm{Fulano} = "São Paulo";          # apenas escalares
+$dbm{Beltrano} = "Belo Horizonte";   # referências seriam gravadas
+$dbm{Ciclano} = "Rio de Janeiro";    # como endereços... inúteis
+untie %dbm; # fecha o banco...
+
+tie %dbm, 'SDBM_File', $arqdbm, O_CREAT|O_RDWR, 0644;
+
+delete $dbm{Ciclano};
+
+foreach (sort keys %dbm) {           # imprime tudo...
+   print "$_ : $dbm{$_}\n";
+}
+
+undef %dbm; # zera o banco de dados !!
+untie %dbm; # fecha o banco...
+#
