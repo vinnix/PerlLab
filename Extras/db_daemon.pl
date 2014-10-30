fc99f4a8849d4f08eab1288d42d7b115a2fa6511
Vinnix Schmidt <vinicius.schmidt@dextra-sw.com>
2014-10-30 12:53:22 -0200

    Começando integração do enviador de email

--- Extras/db_daemon.pl
+++ Extras/db_daemon.pl
@@ -6,6 +6,44 @@ use warnings;
 use App::Daemon qw( daemonize ); 
 use DBI;
 use POSIX;
+use Email::Send::SMTP::Gmail;
+
+
+
+
+my $pfile = "gmail_pass.conf";
+open(ARQ,"< $pfile");
+my $pass = <ARQ>;
+close(ARQ);
+$pass =~ s/\n|^M//g ;
+
+my $gmail_user = 'pganalytics.sender@gmail.com';
+my $version = "Versão: 0.01";
+
+
+
+my ($mail,$error) = Email::Send::SMTP::Gmail->new( -smtp=>'smtp.gmail.com',
+                                                   -login=>$gmail_user,
+                                                   -pass=>$pass);
+
+print "session error: $error" unless ($mail!=-1);
+
+my $att;
+$att->[0]->{file}='./README';
+$att->[1]->{file}='./Attachment_TESTE.txt';
+
+$mail->send(-to=>'dba@dextra-sw.com',
+            -subject=>'[pgAnalytics] Teste de envio de relatório das estatísticas, Alertas, Logs e Monitoramento',
+            -body=>"<h1>Teste</h1><br>Este é um email de teste[ $version ], favor desconsiderar.<br>Novo Teste!",
+            -contenttype=>'text/html',
+            -attachmentlist=>$att);
+
+$mail->bye;
+
+
+
+
+
 
 daemonize();
 
