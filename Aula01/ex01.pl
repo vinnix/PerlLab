72aa3cd3b9ef5a07e4eec89621689be29c9d05b4
Vinnix Schmidt <vinnix.bsd@gmail.com>
2016-01-17 16:43:32 -0200

    Estruturando melhor os comentarios e perdoc tags

--- Aula01/ex01.pl
+++ Aula01/ex01.pl
@@ -2,8 +2,32 @@
 #
 print "Olá Mundo! \n";
 
-$versao = $];
+# Isto é um cmoentário
+#
+#
+
+
+$versao = $]; # $] é uma variável especial que retorna a versão do perl
 
 print "Este PERL está na versão $versao \n";
 print "Obrigado \n";
 
+
+
+
+#######################################################################
+# Informação para o `perldoc`
+#
+
+
+=head1 NAME
+  Nome do Script
+
+
+=head1 VERSION
+
+Version 0.01
+
+
+=head1 SYNOPSIS
+  O que o script faz
